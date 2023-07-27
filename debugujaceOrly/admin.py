8784fbf3d205d4b1079fceedcccef89427fa2b39
tomasdymek <tomasdymek@gmail.com>
2023-07-27 19:01:45 +0200

    Added models in admin.py

--- debugujaceOrly/admin.py
+++ debugujaceOrly/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Company
 
-# Register your models here.
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ['name', 'opening_hour', 'closing_hour']
+    list_filter = ['city']
